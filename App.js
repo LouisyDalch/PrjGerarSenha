3101c2677a0ea6a84059fc5fba16d5e3b0f2c22e
Louisy <louisy0104@gmail.com>
2024-02-04 10:07:33 -0300

    slider

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import {View, Text, StyleSheet,Image} from "react-native"
+import slider from "@react-native-comunity/slider"
 
 export default function /* Componentes têm letra maiúscula */ App(){
   return(
     <View style = {style.container}>
-      <Image
+      <Image style = {style.logo}
       source={require("./rsc/assets/cadeado.png")}
       />
       <Text>
-        Meu primeiro app em react
+        16 caracteres
       </Text>
+      <View style={style.area}>
+
+      </View>
     </View>
   )
 }
@@ -19,5 +23,11 @@ const style = StyleSheet.create({
     backgroundColor: "#5CC9C6",
     justifyContent:"center",
     alignItems:"center"
+  },
+  logo:{
+    marginBottom: "10%"
+  },
+  area:{
+    
   }
 })
