c4303f9d9fc8f9f48596cb99e0bef50d15f6ff42
Louisy <louisy0104@gmail.com>
2024-02-08 22:29:59 -0300

    navegação

---
+++ rsc/pages/password/index.js
@@ -0,0 +1,13 @@
+import {View, Text, StyleSheet} from "react-native"
+
+export function Password(){
+    return(
+        <Text>
+            Minhas senhas
+        </Text>
+    )
+}
+
+const style = StyleSheet.create({
+    
+})
